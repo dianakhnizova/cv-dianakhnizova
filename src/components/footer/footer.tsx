d784dd3763f5243ce1f641cd303c0181c7deb456
Diana Khizhnova <khnizovad@gmail.com>
2025-06-28 15:15:12 +0500

    feat: add footer design

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,7 +1,6 @@
 import styles from "./footer.module.css";
 import { Wrapper } from "../wrapper/wrapper";
 import { messages } from "./messages";
-
 import { PagesLinks } from "@/components/pages-links/pages-links";
 import { Logo } from "../logo/logo";
 import { SocialIcons } from "../social-icons/social-icons";
@@ -14,7 +13,7 @@ export const Footer = () => {
           <Logo />
           <PagesLinks />
         </div>
-        <div className={styles.container}>
+        <div className={styles.bottomContainer}>
           <p className={styles.bottomTitle}>{messages.bottomTitle}</p>
           <SocialIcons />
         </div>
